c70bc584d08cec85b0df88310a3b121b8ccfd18f
Marshall Zobel <dmzobel@gmail.com>
2018-06-12 18:02:34 -0400

    decodeString progress

---
+++ decodeString.js
@@ -0,0 +1,29 @@
+/*
+Given an encoded string, return its corresponding decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is repeated exactly k times. Note: k is guaranteed to be a positive integer.
+
+For s = "4[ab]", the output should be decodeString(s) = "abababab"
+For s = "2[b3[a]]", the output should be decodeString(s) = "baaabaaa"
+*/
+
+const decodeString = string => {
+  let decoded = '';
+  const substring = string.match(/\d.*?\]+/g);
+  if (substring) {
+    const multiplier = +substring[0];
+    const end = substring.length - 1;
+    const newString = substring.slice(2, end);
+
+    for (let i = 0; i < multiplier; i++) {
+      decoded += decodeString(newString);
+    }
+  } else {
+    decoded += string;
+  }
+
+  return decoded;
+};
+
+// console.log(decodeString('4[ab]'));
+decodeString('2[b3[a]]');
