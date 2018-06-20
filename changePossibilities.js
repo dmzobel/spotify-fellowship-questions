afe10a42aa1307cf49c69a64bb80cffecd714894
Marshall Zobel <dmzobel@gmail.com>
2018-06-20 11:34:42 -0400

    changePossibilities refactor

--- changePossibilities.js
+++ changePossibilities.js
@@ -11,10 +11,37 @@ Example: for amount=4 (4¢) and denominations=[1,2,3] (1¢, 2¢ and 3¢), your p
 2¢, 2¢
 */
 
-function changePossibilities(amt, denomArray) {
-  let combinations = 0;
+// memoize the possibilities
+const memo = {};
+
+function changePossibilities(target, denomArray) {
+  const key = `${target}:${denomArray}`;
+
+  if (memo[key]) return memo[key];
+  else {
+    let combinations = 0;
+    const sorted = denomArray.sort((a, b) => a - b); // sorts in ascending order
+
+    for (let i = sorted.length - 1; i >= 0; i--) {
+      const denom = sorted[i];
+
+      if (denom === target) combinations++;
+      else if (denom < target) {
+        if (target % denom === 0) {
+          combinations++;
+        }
+        const diff = denom - target;
+        combinations += changePossibilities(
+          diff,
+          sorted.slice(0, sorted.length - 1)
+        );
+      }
+    }
+
+    memo[key] = combinations;
+    return combinations;
+  }
   // need to check all additions
   // need to check if any multiplier of the denomination equals the amount
   // need to check if any multiplier of one denomination plus any multiplier of ANY of the other denominations equals the amount
-  return combinations;
 }
